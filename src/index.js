2126e654559188885fb7da41e2ed51079f14cc2e
Dmitriy <dvk.09@mail.ru>
2017-11-15 14:32:51 +0300

    ServiceWorker

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import registerServiceWorker from './registerServiceWorker';
 import "./index.css"
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+registerServiceWorker();
